47b9ca263e702d80a4321bcf96489af64b77314b
codec-onion <bonnetmatthieu86@gmail.com>
2023-11-17 20:03:33 +0100

    update app.js for vercel

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app
   .use(express.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization")
+    res.setHeader("Access-Control-Allow-Headers","X-Requested-With, Content, Accept, Content-Type, Authorization")
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
     next()
   })
