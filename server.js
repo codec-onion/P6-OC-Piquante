1c81ad05d56add5538c6e609348837ab55ddc944
codec-onion <bonnetmatthieu86@gmail.com>
2023-11-17 20:13:06 +0100

    update app.js for vercel

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const normalizePort = val => {
   return false;
 };
 const port = normalizePort(process.env.PORT || '3000');
-set('port', port);
+app.set('port', port);
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
